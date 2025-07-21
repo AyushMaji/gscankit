27c5625e841e55b0334baf3a1b65f47048233250
ayushfortmindz <ayushmaji2001@gmail.com>
2025-07-21 13:21:38 +0530

    Rename gscancore.dart to gscankit.dart
    Updated import statements and renamed the core library file to improve naming consistency across the project.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gscankit/gscancore.dart';
+import 'package:gscankit/gscankit.dart';
 
 void main() {
   runApp(const MyApp());
